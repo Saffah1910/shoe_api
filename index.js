c6dc942983226e5a378b720c95b01b70f69fac43
Saffah Khatib <skhatib1910@gmail.com>
2023-11-03 15:34:48 +0200

    connection string

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import 'dotenv/config';
 
 const connectionString = process.env.DATABASE_URL;
 const pgp = pgPromise({});
-const db = pgp(connectionString);
+// const db = pgp(connectionString);
 
 const app = express();
 
